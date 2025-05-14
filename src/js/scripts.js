74cc0d55db5b7a05f7cc86ff548af0c65c07ad08
mrgxx77 <margovlada123@email.com>
2025-05-14 19:20:03 +0300

    github

---
+++ src/js/scripts.js
@@ -0,0 +1,15 @@
+// const dark = document.querySelector(".dark-bgc"),
+//       burger = document.querySelector(".burger"),
+//       menu = document.querySelector(".menu"),
+//     //   cancel = document.querySelector(".cancel")
+
+// burger.addEventListener("click", function() {
+//   menu.style.left = "0";
+//   dark.style.display = "block"
+// })
+// function cancelBurger() {
+//   menu.style.left = "-100%";
+//   dark.style.display = "none"
+// }
+// // cancel.addEventListener("click", cancelBurger)
+// dark.addEventListener("click", cancelBurger)
